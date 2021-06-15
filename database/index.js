945d44ca70cf3500f782395e9490844d350f6805
ibnusyawall <website.isas.24@gmail.com>
2021-06-15 16:26:36 +0700

    test

--- database/index.js
+++ database/index.js
@@ -1,5 +1,8 @@
+'use strict';
+
 const moment = require('moment-timezone')
 const nedb = require('nedb')
+const _ = require('lodash')
 
 var db = {}
 
@@ -11,20 +14,64 @@ class Database {
         this.db.siswa.loadDatabase()
     }
 
-    addClass(nama_kelas, siswa) {
+    addClass(nama_kelas, siswa = {}) {
         return new Promise(async (resolve, reject) => {
-            let data = { id: this.randomId, kelas: nama_kelas, data_siswa: siswa }
-            let result = this.db.siswa.insert(data)
-            resolve(result)
+            this.checkNameClassFromDb(nama_kelas)
+                .then(async s => {
+                    let data = { kelas: nama_kelas, data_siswa: siswa, absen: {} }
+                    await this.db.siswa.insert(data)
+                    resolve({ status: true, message: 'data berhasil dimasukan.' })
+                })
+                .catch(err => {
+                    reject({ status: false, message: 'nama kelas sudah terdaftar.' })
+                })
+        })
+    }
+
+    findClass(_id) {
+        return new Promise(async (resolve, reject) => {
+            
+        })
+    }
+    insertSiswa(_id, siswa = {}) {
+        return new Promise((resolve, reject) => {
+            this.db.siswa.findOne({ _id: _id }, (e, doc) => {
+                if (!doc) reject({ status: false, message: 'id kelas tidak ditemukan.' })
+                this.db.siswa.update({ _id: _id }, { $set: { 'data_siswa': { ...siswa }} }, {}, (e, docs) => {
+                    resolve({ status: true, message: 'siswa baru telah ditambahkan', data: doc })
+                })
+            })
+        })
+    }
+
+    checkNameClassFromDb(name) {
+        return new Promise((resolve, reject) => {
+            this.db.siswa.find({ kelas: name }, (e, doc) => {
+                _.isEmpty(doc) ? resolve(true) : reject(false)
+            })
         })
     }
 
     getIdFromNameClass(name) {
-
+        return new Promise((resolve, reject) => {
+            this.db.siswa.findOne({ kelas: name }, (e, doc) => {
+                doc ?
+                    resolve({
+                        status: true,
+                        kelas: name,
+                        id: doc._id
+                    }) :
+                    reject({
+                        status: false,
+                        message: 'nama kelas tidak ditemukan.'
+                    })
+            })
+        })
     }
-    updateNameClass(id, name) {
+
+    updateNameClass(_id, name) {
         return new Promise((resolve) => {
-            this.db.siswa.find({ id: id }, (e, doc) => {
+            this.db.siswa.find({ _id: _id }, (e, doc) => {
                 resolve(doc)
             })
         })
@@ -36,12 +83,28 @@ class Database {
 }
 
 
-// new Database().addClass('XI RPL C', ['Ibnu', 'Fahri', 'Dafi', 'Ajid', 'Fajar'])
+// new Database().addClass('XI RPL A', { nama: 'Ibnu', no: '6282299265151' })
+//     .then(s => console.log(s))
+//     .catch(err => console.log(err))
+
+
+
+// new Database().updateNameClass("id_9d228cb68dbb7")
 //     .then(s => console.log(s))
 //     .catch(err => console.log)
 
 
+// new Database().getIdFromNameClass("XI RPL d")
+//     .then(s => console.log(s))
+//     .catch(err => console.log(err))
 
-new Database().updateNameClass("id_9d228cb68dbb7")
+
+// new Database().checkNameClassFromDb("XI RPL SDD")
+//     .then(s => console.log(!s))
+//     .catch(err => console.log(err))
+
+
+
+new Database(). insertSiswa("w9b3GOqt9wi7ODCR", { name: 'Dafi', no: '6287865437894'})
     .then(s => console.log(s))
-    .catch(err => console.log)
+    .catch(err => console.log(err))
